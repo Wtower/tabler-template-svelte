f55725136e2c6382e26b9ab5ed06e1d1a6e7d357
George Karakostas <gkarak@9-dev.com>
2023-02-25 11:04:26 +0200

    Added svelte transition to offcanvas

--- src/lib/interface/offcanvas.svelte
+++ src/lib/interface/offcanvas.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { fade } from "svelte/transition";
+
     /** 
      * The offcanvas label.
      * @type {string} 
@@ -26,29 +28,31 @@ Slots
 - default
 - buttons
 -->
-<div class="offcanvas offcanvas-bottom" 
-    class:show
-    tabindex="-1" 
-    aria-labelledby={label}>
-    <div class="offcanvas-header">
-        <h2 class="offcanvas-title">{label}</h2>
-        {#if closable}
-            <button type="button"
-                class="btn-close text-reset"
-                aria-label="Close"
-                on:click={() => {show = false}}>
-            </button>
-        {/if}
-    </div>
-    <div class="offcanvas-body">
-        <div><slot /></div>
-        <div class="mt-3">
-            <slot name="buttons">
-                <button class="btn btn-primary" type="button" on:click={() => {show = false}}>
-                    Close
+{#if show}
+    <div class="offcanvas offcanvas-bottom show" 
+        transition:fade
+        tabindex="-1" 
+        aria-labelledby={label}>
+        <div class="offcanvas-header">
+            <h2 class="offcanvas-title">{label}</h2>
+            {#if closable}
+                <button type="button"
+                    class="btn-close text-reset"
+                    aria-label="Close"
+                    on:click={() => {show = false}}>
                 </button>
-            </slot>
+            {/if}
+        </div>
+        <div class="offcanvas-body">
+            <div><slot /></div>
+            <div class="mt-3">
+                <slot name="buttons">
+                    <button class="btn btn-primary" type="button" on:click={() => {show = false}}>
+                        Close
+                    </button>
+                </slot>
+            </div>
         </div>
     </div>
-</div>
-<div class="offcanvas-backdrop fade" class:show></div>
+    <div class="offcanvas-backdrop show" transition:fade></div>
+{/if}
