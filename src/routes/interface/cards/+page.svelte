20920a5035251bab08298ce20e8b3eaadba84703
George Karakostas <gkarak@9-dev.com>
2023-03-01 10:47:13 +0200

    Added cards demo page

---
+++ src/routes/interface/cards/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { base } from '$app/paths';
+	import Empty from "$lib/interface/empty.svelte";
+    import { config } from "$stores/config";
+</script>
+
+<Empty path="{base}/interface/cards" {...$config}>
+    <span slot="h2">Cards</span>
+</Empty>
--- src/stores/config.js
+++ src/stores/config.js
@@ -29,7 +29,14 @@ export const config = readable({
         }, {
             href: base + '/interface/badges',
             title: 'Badges',
+            badge: {text: 'NEW'},
+        }, {
+            title: 'Cards',
             badge: {color: 'green', text: 'NEW'},
+            menu: [[{
+                href: base + '/interface/cards',
+                title: 'Sample cards',
+            }]],
         }], [{ // 2nd column
             title: 'Sub-menu',
             menu: [[{
@@ -42,9 +49,6 @@ export const config = readable({
                 icon: '<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3" /><line x1="12" y1="12" x2="20" y2="7.5" /><line x1="12" y1="12" x2="12" y2="21" /><line x1="12" y1="12" x2="4" y2="7.5" /><line x1="16" y1="5.25" x2="8" y2="9.75" /></svg>',
                 disabled: true,
             }]],
-        }, {
-            title: 'Sub-menu #2',
-            badge: {text: 'New'},
         }]],
     }, {
         href: base + '/form-elements',
